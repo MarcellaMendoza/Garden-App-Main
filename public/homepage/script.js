5cad9837cfc4a2cc0f4b5561bc32c1aa60992e2d
MarcellaMendoza <99848492+MarcellaMendoza@users.noreply.github.com>
2022-05-13 02:24:22 +0000

    Updates on home page, location page, and facts page.

--- public/homepage/script.js
+++ public/homepage/script.js
@@ -32,3 +32,13 @@ function hideAllSlides() {
 }
 
 
+
+
+
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navbarLinks = document.getElementsByClassName('navigation')[0]
+
+toggleButton.addEventListener('click', () => {
+  navbarLinks.classList.toggle('active')
+})
+
