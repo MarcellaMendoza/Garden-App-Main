9e585d7893db7a138ac025916cc5430ee3b41d10
Marcella Mendoza <99848492+MarcellaMendoza@users.noreply.github.com>
2022-05-25 20:53:22 +0000

    Up to date

--- public/homepage/script.js
+++ public/homepage/script.js
@@ -34,11 +34,3 @@ function hideAllSlides() {
 
 
 
-
-const toggleButton = document.getElementsByClassName('toggle-button')[0]
-const navbarLinks = document.getElementsByClassName('navigation')[0]
-
-toggleButton.addEventListener('click', () => {
-  navbarLinks.classList.toggle('active')
-})
-
